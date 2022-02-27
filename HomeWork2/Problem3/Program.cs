9f877accdc1a0234b28877d45e29b6bafbbb8867
Bark-1 <2762259669@qq.com>
2022-02-27 09:17:31 +0800

    second

---
+++ HomeWork2/Problem3/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Problem3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PrimeNum(out bool[] isValid, 100);
+            for(int i=1;i<=100;++i)
+            {
+                if (isValid[i]) Console.WriteLine(i);
+            }
+        }
+        static void PrimeNum(out bool[] isPrime,int num)
+        {
+            isPrime = new bool[num + 1];
+            for(int i=0;i<=num;++i)
+            {
+                isPrime[i] = true;
+            }
+            for(int i=2;i<=num;++i)
+            {
+                for(int j=i+1;j<=num;++j)
+                {
+                    if(j%i==0)
+                    {
+                        isPrime[j] = false;
+                    }
+                }
+            }
+        }
+    }
+}
