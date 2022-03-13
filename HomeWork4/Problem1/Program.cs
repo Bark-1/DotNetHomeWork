f75cc5d0fa2e6494463b8f64de6eb6167519982e
Bark-1 <2762259669@qq.com>
2022-03-13 09:50:18 +0800

    4

---
+++ HomeWork4/Problem1/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+
+namespace Problem1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {            
+            GenericList<int> glist = new GenericList<int>();
+            for(int i=1;i<10;++i)
+            {
+                glist.Add(i);
+            }
+            //打印
+            Console.WriteLine("打印链表元素");
+            glist.ForEach(m => Console.WriteLine(m));
+            //求最大值
+            int max = int.MinValue;
+            glist.ForEach(m =>max=Math.Max(max,m));
+            Console.WriteLine($"最大值：{max}");
+            //求最小值
+            int min = int.MaxValue;
+            glist.ForEach(m =>min=Math.Min(min, m));
+            Console.WriteLine($"最小值：{min}");
+            //求和
+            int sum = 0;
+            glist.ForEach(m=>sum+=m);
+            Console.WriteLine($"和：{sum}");
+        }
+    }
+    public class Node<T>
+    {
+        public Node<T> Next { get; set; }
+        public T Data { get; set; }
+        public Node(T t)
+        {
+            Next = null;
+            Data = t;
+;        }
+    }
+    public class GenericList<T>
+    {
+        private Node<T> head;
+        private Node<T> tail;
+        public GenericList()
+        {
+            tail = head = null;
+        }
+        public Node<T> Head { get => head; }
+        public void Add(T t)
+        {
+            Node<T> n = new Node<T>(t);
+            if(tail==null)
+            {
+                head = tail = n;
+            }
+            else
+            {
+                tail.Next = n;
+                tail = n;
+            }
+        }
+        public void ForEach(Action<T> action)
+        {
+            for(Node<T> node = head; node != null; node = node.Next)
+            {
+                action(node.Data);
+            }
+        }
+    }
+}
---
+++ HomeWork4/Problem2/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+
+namespace Problem2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person person = new Person();
+            person.SetAlarmTime(5);
+            person.StartClock();
+            
+            while (true) { };
+        }
+
+    }
+    public delegate void AlarmHandler(object sender, int args);
+
+    public class Clock
+    {
+        public int AlarmTime { set; get; }
+        private int CurrentTime = 0;
+        private System.Timers.Timer timer;
+        public event AlarmHandler OnAlarm;
+        public Clock()
+        {
+            timer = new System.Timers.Timer(1000);
+            timer.AutoReset = true;
+            timer.Elapsed += new System.Timers.ElapsedEventHandler(Tick);
+            timer.Enabled = true;
+        }
+        public void StartTimer()
+        {
+            timer.Start();
+        }
+        //Tick不用person订阅。是clock内部事件。当person调用StartClock时开始。当时间到时触发Alarm
+        public void Tick(object sender,System.Timers.ElapsedEventArgs e)
+        {
+            CurrentTime++;
+            Console.WriteLine($"Tick! CurrentTime:{CurrentTime}");
+            if (CurrentTime == AlarmTime) { OnAlarm(this, CurrentTime); }
+        }
+    }
+
+    public class Person
+    {
+        public Clock clock;
+        public Person()
+        {
+            clock = new Clock();
+            clock.OnAlarm += ClockOnAlarm;
+        }
+        public void SetAlarmTime(int time)
+        {
+            clock.AlarmTime = time;
+        }
+        public void StartClock()
+        {
+            clock.StartTimer();
+        }
+        void ClockOnAlarm(object sender, int time)
+        {
+            Console.WriteLine($"Alarm! CurrentTime:{time}");
+        }
+    }
+}
